955598344feec47806063a5ee321cf233e714d74
20chen-7 <xiaoyuchen007@gmail.com>
2024-06-19 16:08:02 -0600

    style and fix logic

--- src/components/CodingChallenge.vue
+++ src/components/CodingChallenge.vue
@@ -1,8 +1,9 @@
 <template>
   <div>
-    <h1>Options Profit Calculator</h1>
+    <h1>
+      Options Profit Calculator</h1>
     <div class="row">
-      <div class="col mb-6 strike-list">
+      <div class="col-mb-12 col-lg-12 strike-list">
         <table>
           <thead>
             <tr class="table-header">
@@ -13,7 +14,8 @@
               <th>Ask</th>
               <th>Position</th>
               <th>Expiration Date</th>
-              <th>Chose</th>
+              <th>Select</th>
+              <th>#Contract</th>
             </tr>
           </thead>
           <tbody class="table-body">
@@ -26,13 +28,16 @@
               <td>{{ option.long_short }}</td>
               <td>{{ new Date(option.expiration_date).toLocaleDateString() }}</td>
               <td>
-                <input type="checkbox" v-model="option.checked" :id="option.strike_price" @click.stop />
+                  <input type="checkbox" v-model="option.checked" :id="option.strike_price" @click.stop />
+              </td>
+              <td>
+                <input type="number" v-model="option.number" :id="option.strike_price"/>
               </td>
             </tr>
           </tbody>
         </table>
       </div>
-      <div class="col mb-6 strike-visualization">
+      <div class="col-mb-6 col-lg-6 strike-visualization">
         <div class="sliders-container">
           <div class="slider-container">
             <input
@@ -54,6 +59,16 @@
             />
             <span>Max Price: {{maxPriceVal}}</span>
           </div> 
+          <div class="slider-container">
+            <input
+              type="range"
+              v-model="stepVal"
+              min="0.1"
+              max="10"
+              @input="updateStepVal($event.target.value)"
+            />
+            <span>Step Change: {{stepVal}}</span>
+          </div> 
         </div>
         <div>
           <button @click="calculate">Generate</button>
@@ -63,36 +78,44 @@
           <line-chart v-if="loaded" :data="chartData" :options="options"></line-chart>
         </div>
       </div>
-      <div class="profit-summary mb-12">
-        <div class="card-header">
-          <slot name="header">Max Profit:</slot>
+      <div v-if="loaded" class="summary">
+        <div class="card-container mb-4">
+          <div class="card-header">
+            <slot name="header">Max Profit:</slot>
+          </div>
+          <div class="card-body">
+            <div v-for="summary in summaryArray" :key="summary.label">{{ summary.label }}: {{ summary.maxProfit }}</div>
+          </div>
+          <div class="card-footer">
+            <slot name="footer"></slot>
+          </div>
         </div>
-        <div class="card-body">
-          <slot name="body">{{ maxProfit }}</slot>
+        <div class="card-container mb-4">
+          <div class="card-header">
+            
+            <slot name="header">Max Loss</slot>
+          </div>
+          <div class="card-body">
+            <div v-for="summary in summaryArray" :key="summary.label">{{ summary.label }}: {{ summary.maxLoss }}</div>
+          </div>
+          <div class="card-footer">
+            <slot name="footer"></slot>
+          </div>
         </div>
-        <div class="card-footer">
-          <slot name="footer"></slot>
+        <div class="card-container mb-4">
+          <div class="card-header">
+            <slot name="header">Break Even Points</slot>
+          </div>
+          <div class="card-body">
+            <div v-for="summary in summaryArray" :key="summary.label">{{ summary.label }}: {{summary.breakEvenPoint}}</div>
+          </div>
+          <div class="card-footer">
+            <slot name="footer"></slot>
+          </div>
         </div>
-       </div>
-       <div class="card-header">
-          <slot name="header">Max Loss</slot>
-        </div>
-        <div class="card-body">
-          <slot name="body">{{ maxLoss }}</slot>
-        </div>
-        <div class="card-footer">
-          <slot name="footer"></slot>
-        </div>
-       </div>
-       <div class="card-header">
-          <slot name="header">Break Even Points</slot>
-        </div>
-        <div class="card-body">
-          <slot name="body">{{ breakEvenPoints.join(', ') }}</slot>
-        </div>
-        <div class="card-footer">
-          <slot name="footer"></slot>
-        </div>
+      </div>
+     </div> 
+    
     </div>
 </template>
 <script src="https://cdn.jsdelivr.net/npm/chart.js"></script>
@@ -100,7 +123,7 @@
 import { Line } from 'vue-chartjs'
 import { Chart as ChartJS, Title, Tooltip, Legend, LineElement, PointElement, LinearScale, CategoryScale } from 'chart.js'
 ChartJS.register(Title, Tooltip, Legend, LineElement, PointElement, LinearScale, CategoryScale)
-
+import { FontAwesomeIcon } from '@fortawesome/vue-fontawesome'
 export default {
   name: 'CodingChallenge',
   props: {
@@ -114,6 +137,9 @@ export default {
     this.optionsData.forEach((option,index)=>{
       datasets.push({});
       });
+      this.optionsData.forEach((option,index)=>{
+        option.number = 1;
+      });
     return {
       chartData: {
         labels: [],
@@ -123,16 +149,39 @@ export default {
       loaded: false,
       min: 0,
       max: 250,
-      minPriceVal: 75,
+      minPriceVal:  75,
       maxPriceVal: 125,
+      stepVal: 0.5,
+      summaryArray: [],
+      breakEvenPoints: [],
   }}, 
   
   methods: {
+    fetchedBreakEvenData(){
+      const breakEvenPoints = [];
+      if(this.optionsData.length){
+        for(let index = 0; index < this.optionsData.length; index++){
+          let option = this.optionsData[index];
+          let point;
+          let premium = (option.ask + option.bid)/2;
+          if(option.long_short === 'long'){
+            point = option.type === 'call' ? option.strike_price + premium : option.strike_price - premium;
+          }else{
+            point = option.type === 'call' ? option.strike_price + premium : option.strike_price - premium;
+          }
+          breakEvenPoints.push(point);
+        }
+      }
+      this.breakEvenPoints = breakEvenPoints;
+    },
     updateMinVal(val){
       this.minPriceVal = Math.max(this.min, val);
     },
     updateMaxVal(val){
       this.maxPriceVal = Math.min(this.max, val);
+    },
+    updateStepVal(val){
+      this.stepVal = val;
     },
 
     toggleCheckbox(index){
@@ -143,11 +192,16 @@ export default {
       const colors = ['#f87979', "yellow", "red", "green"]
       
       this.optionsData.forEach((option,index)=>{
-        profits.push({id: index, data:[],color:colors[index]})
+        profits.push({id: index, data:[],color:colors[index], breakEvenPoint:this.breakEvenPoints[index]})
       });
-      const totalProfits = []
+      const totalProfits = [];
       const prices = [];
-      for (let price = this.minPriceVal; price <= this.maxPriceVal; price+=0.50){
+      const totalBreakEvenPoint = [];
+      this.optionsData.filter(option=>option.checked).forEach((option,index)=>{
+        totalBreakEvenPoint.push(this.breakEvenPoints[index]);
+      })
+
+      for (let price = this.minPriceVal; price <= this.maxPriceVal; price+=this.stepVal){
         prices.push(price);
         let totalProfit = 0;
         for(let index = 0; index < this.optionsData.length; index++){
@@ -167,6 +221,9 @@ export default {
           }else{
             optionProfit = option.bid - intrinsicVal;
           }
+          if(option.number !== 1){
+            optionProfit *= option.number;
+          }
           totalProfit += optionProfit;
           if (!profits[index].data) {
             profits[index].data = [];
@@ -180,6 +237,7 @@ export default {
         label: 'Total Profit/Loss',
         backgroundColor: 'blue', 
         data: totalProfits,
+        breakEvenPoint: totalBreakEvenPoint,
         fill: false,
         tension: 0.2,
         pointRadius: 2,
@@ -189,6 +247,7 @@ export default {
         label: `No. ${item.id+1}`,
         backgroundColor: item.color,
         data: item.data,
+        breakEvenPoint: item.breakEvenPoint,
         fill: false,
         tension: 0.2,
         pointRadius: 2,
@@ -200,11 +259,27 @@ export default {
       };
       
     },
-    customAlert(options) {
+    customAlert(options){
       return new Promise((resolve) => {
           alert(`${options.title}\n\n${options.message}`);
           resolve();
         });
+    },
+    summary(){
+      if (!this.chartData.datasets.length) {
+        return [];
+      }
+      this.summaryArray = this.chartData.datasets.map((dataset, index) => {
+        if (Array.isArray(dataset.data) && dataset.data.length) {
+          return {
+            label:dataset.label,
+            maxProfit: Math.max(...dataset.data),
+            maxLoss: Math.min(...dataset.data),
+            breakEvenPoint: dataset.breakEvenPoint,
+          };
+        }
+      });
+      
     },
     calculate(){
       if (!this.optionsData.filter(option => option.checked).length) {
@@ -214,52 +289,62 @@ export default {
         });
       }else{
         this.loaded = true;
+        this.fetchedBreakEvenData();
         this.generateChartData();
+        this.summary();
       }
-    }
-  },
-  computed: {
-    maxProfit() {
-      if (!this.chartData || !this.chartData.datasets || !this.chartData.datasets[0].data) {
-      return 0; 
-    }
-      return Math.max(...this.chartData.datasets[0].data);
     },
-    maxLoss() {
-      if (!this.chartData || !this.chartData.datasets || !this.chartData.datasets[0].data) {
-      return 0; 
-    }
-      return Math.min(...this.chartData.datasets[0].data);
-    },
-    breakEvenPoints() {
-      if (!this.chartData || !this.chartData.datasets || !this.chartData.datasets[0].data) {
-      return [] 
-    }
-      return this.chartData.labels.filter((_, index) => this.chartData.datasets[0].data[index] === 0);
-    }
+    
   },
 }
 </script>
 
 <style scoped>
-h1 {
+div{
   text-align: center;
+}
+h1 {
   color: #19222b;
+  font-size: 2rem;
   font-family: fantasy;
   font-weight: bolder;
-  background-color: beige;
-  border: 2mm none rgba(213, 152, 38, 0.6);
-  padding: 10px;
-  box-shadow: 10px 10px 10px 5px rgb(174, 174, 159);
-  border-radius: 20px;
-  margin: 20px auto;
+}
+.row {
+  display: flex;
+  flex-wrap: wrap;
+  margin-right: -15px;
+  margin-left: -15px;
+}
+
+.col-lg-12, .col-md-12 {
+  position: relative;
+  width: 100%;
+  padding-right: 15px;
+  padding-left: 15px;
+}
+@media (min-width: 768px) { /* Adjusts for medium screens and up */
+  .col-md-6 {
+    flex: 0 0 50%;
+    max-width: 50%;
+  }
+}
+
+@media (min-width: 992px) { /* Adjusts for large screens */
+  .col-lg-6 {
+    flex: 0 0 50%;
+    max-width: 50%;
+  }
+}
+.strike-list{
+  align-items: center;
 }
 table{
   font-family: sans-serif;;
-  margin: 0px auto;
+  font-size: 1rem;
   border-collapse: collapse;
   width: 80%;
   color: white;
+  margin: 10% auto;
   background: -webkit-linear-gradient(left, rgb(127, 26, 26) 0%, rgb(137, 18, 18) 20%, rgb(137, 18, 18) 60%, rgb(43, 22, 136) 100%);
 }
 
@@ -271,6 +356,21 @@ thead{
 tr:hover {
   background: -webkit-linear-gradient(left, rgb(144, 178, 124) 0%, rgb(144, 178, 124) 20%, rgb(144, 178, 124) 80%, rgb(183, 143, 63) 100%);
 }
+input[type=number]{
+  font-size: larger;
+  text-align: center;
+  color: white;
+  background-color: transparent;
+  border: none;
+  padding: 0;
+  margin: 0;
+  width: 100%
+}
+
+input[type=number]:focus {
+  background-color: lightblue;
+}
+
 .strike-visualization{
   align-items: center;
 }
@@ -293,29 +393,16 @@ button{
   margin-left: 10%;
 }
 .sliders-container {
-  display: flex;
-  flex: 0 0 100vw;
-  flex-direction: column;
-  flex-wrap: wrap;
-  align-items: center;
   justify-content: center;
-  align-content: center;
 }
 .slider-container {
-  margin: 10px;
-}
-.slider {
-  display: flex;
-  justify-content: flex-start;
-}
-
-input[type="range"] {
-  margin-right: 10px;
-  flex: 1;
+  margin: 30px;
 }
 span {
-  width: 100px;
-  text-align: center;
+  display: inline-block;
+  text-align: left;
+  width: 10%;
+  margin-left: 5%;
 }
 
 .table-header{
@@ -323,5 +410,34 @@ span {
 }
 .row{
   text-align: center;
+}
+.summary{
+  display: flex; 
+  justify-content: center;
+  gap: 20px;
+  padding: 20px;
+  flex-direction: row;
+}
+.card-container {
+  border: 1px solid #e0e0e0;
+  border-radius: 8px;
+  box-shadow: 0 2px 5px rgba(0, 0, 0, 0.1);
+  overflow: hidden;
+  background-color: white;
+  width: 40ch;
+}
+.card-header{
+  color: white;
+  font-weight: bold;
+  background-color: rgb(67, 53, 131);
+};
+
+.card-footer {
+  padding: 16px;
+  background-color: #f5f5f5;
+}
+
+.card-body {
+  padding: 16px;
 }
 </style>
